26fa4b99891f641f8307c54757502603e68c43fd
Yansel Martinez <ymartinez7@gmail.com>
2022-03-23 16:00:26 +0100

    Creación de los poryectos consumer y producer

---
+++ src/NetRabbitMQ.ConsumerApp/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var _factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "ymartinez7",
+    Password = "y18010611m"
+};
+
+using (var connection = _factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("TestQueue", false, false, false, null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.Span;
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine("Mesaje recibido {0}", message);
+        };
+
+        channel.BasicConsume("TestQueue", true, consumer);
+
+        Console.WriteLine("Presiona [enter] para salir del consumer");
+        Console.ReadLine();
+    }
+}
---
+++ src/NetRabbitMQ.ProducerApp/Sender.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var _factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName= "ymartinez7",
+    Password = "y18010611m"
+};
+
+using (var connection = _factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("TestQueue", false, false, false, null);
+
+        var message = "Bienvenido al proyecto RabbitMQ y .NET";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(String.Empty, "TestQueue", null, body);
+
+        Console.WriteLine("El mensaje fue enviado {0}", message);
+    }
+}
+
+Console.WriteLine("Presiona [enter] para salir de la aplicación");
+Console.ReadLine();
